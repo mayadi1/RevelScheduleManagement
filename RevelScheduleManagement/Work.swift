75d5f2b4882705f82df16393e647bb679485f842
Keita Ito <keitaitok@gmail.com>
2016-09-09 00:21:17 -0700

    Add CLWeeklyCalendarView

---
+++ RevelScheduleManagement/Work.swift
@@ -0,0 +1,16 @@
+//
+//  Work.swift
+//  RevelScheduleManagement
+//
+//  Created by Keita Ito on 9/9/16.
+//  Copyright © 2016 Mohamed Ayadi. All rights reserved.
+//
+
+import Foundation
+
+struct Work {
+    let employee: String
+    let id: Int
+    let shift_begin_time: String
+    let shift_end_time: String
+}
