a388a41c47f6bf3f934ab6086e206c2d09209375
Mohamed Ayadi <mayadi1@mail.ccsf.edu>
2016-09-08 21:36:27 -0700

    Initial Commit

---
+++ RevelScheduleManagement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RevelScheduleManagement
+//
+//  Created by Mohamed Ayadi on 9/8/16.
+//  Copyright © 2016 Mohamed Ayadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
